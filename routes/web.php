72d8c281845de0ae365c08ba0c7cb31b5e5ef1d3
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2025-05-24 18:13:39 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     // Rute untuk profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
---
+++ tugas10.md
@@ -0,0 +1,8 @@
+1.Browser Register
+![alt text](image-20.png)
+2.Browser Verify Email
+![alt text](image-21.png)
+3.Browser Mailtrap
+![alt text](image-22.png)
+4.Browser Dashboard
+![alt text](image-23.png)
